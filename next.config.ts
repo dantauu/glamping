1de499da7a499ea16c496191ebe150eb73275b4a
dantauu <azazzzz455u@gmail.com>
2025-02-08 23:03:20 +0300

    fiix ?

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  "compilerOptions": {
-    "strict": true,
-    "esModuleInterop": true,
-    "lib": ["dom", "dom.iterable", "esnext"],
-    "skipLibCheck": true
-  }
+	compilerOptions: {
+		strict: true,
+		esModuleInterop: true,
+		lib: ['dom', 'dom.iterable', 'esnext'],
+		skipLibCheck: true,
+	},
+	eslint: {
+		ignoreDuringBuilds: true,
+	},
+	images: {
+		disableStaticImages: true,
+	},
 }
 
 export default nextConfig;
