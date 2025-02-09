35cd0f7be406a4a373df72585424c78d510aa075
dantauu <azazzzz455u@gmail.com>
2025-02-09 21:52:13 +0300

    main page 90%

---
+++ src/features/features-home/question/question.tsx
@@ -0,0 +1,33 @@
+import DropQuestion from '@/shared/ui/drop-question/drop-question';
+import style from './question.module.scss';
+import { useState } from 'react';
+
+const Question = () => {
+    return (
+			<div className={style.questionWrapper}>
+				<div className={style.questionLeft}>
+					<div className={style.title}>
+						<h1 className={style.titleInner}>Часто задаваемые вопросы</h1>
+					</div>
+					<div className=''>
+						<img src={'/assets/img/question.svg'} alt='' />
+					</div>
+				</div>
+                <div className="">
+				<div className={style.column}>
+					<DropQuestion title={'Как забронировать тур онлайн?'} />
+					<DropQuestion title={'Как проверить статусзаза?'} />
+					<DropQuestion title={'Как запросить дополнительные услуги(ранний/поздный выезд, кроватку для ребёнка, заселение с животным)?'} />
+					<DropQuestion title={'Как забронировать тур онлайн?'} />
+				</div>
+                <div className={style.questionButton}>
+                    <button className={style.button}>
+                        Раскрыть список
+                    </button>
+                </div>
+                </div>
+			</div>
+		)
+}
+
+export default Question
