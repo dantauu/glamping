b11e554a08fba87dacd57ab81a46d773ae910131
dantauu <azazzzz455u@gmail.com>
2025-02-16 04:58:37 +0300

    update

--- src/intities/intities-personal-account-lk/not-archive/not-archive.tsx
+++ src/intities/intities-personal-account-lk/not-archive/not-archive.tsx
@@ -1,10 +1,33 @@
-
+import style from './not-archive.module.scss'
 
 const NotArchive = () => {
     return (
-        <div className="">
-            NotArchive
+        <div className={style.mainWrapper}>
+        <div className={style.wrapper}>
+            <div className={style.titleIcon}>
+                <img src={'/assets/img/glamping.svg'} alt="" />
+            </div>
+            <div className={style.titleText}>
+                <div className="">
+                    <h2 className={style.titleTextInner}>
+                        У вас пока небыло бронирований
+                    </h2>
+                </div>
+                <div className={style.description}>
+                    <p className={style.descriptionInner}>
+                        Завершенные бронирования будут отображаться в этом разделе
+                    </p>
+                </div>
+            </div>
+            <div className={style.button}>
+                <div className={style.buttonItem}>
+                    <button className={style.buttonInner}>
+                        Подобрать место для отдыха
+                    </button>
+                </div>
+            </div>
         </div>
+    </div>
     )
 }
 
--- src/intities/intities-personal-account-lk/not-booking/not-booking.tsx
+++ src/intities/intities-personal-account-lk/not-booking/not-booking.tsx
@@ -1,10 +1,29 @@
-
+import style from './not-booking.module.scss'
 
 const NotBooking = () => {
     return (
-        <div className="">
-            NotBooking
+      <div className={style.mainWrapper}>
+        <div className={style.wrapper}>
+            <div className={style.titleIcon}>
+                <img src={'/assets/img/glamping.svg'} alt="" />
+            </div>
+            <div className={style.titleText}>
+                <h2 className={style.titleTextInner}>
+                    У вас пока нет предстоящих бронирований
+                </h2>
+            </div>
+            <div className={style.button}>
+                <div className={style.buttonIcon}>
+                    <img src={'/assets/img/search.svg'} alt="" />
+                </div>
+                <div className={style.buttonItem}>
+                    <button className={style.buttonInner}>
+                        Подобрать место для отдыха
+                    </button>
+                </div>
+            </div>
         </div>
+    </div>
     )
 }
 
