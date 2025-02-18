ab2fd93b70d204b95be8fd03283fc3d2e05929b5
dantauu <azazzzz455u@gmail.com>
2025-02-18 14:07:37 +0300

    start

--- src/features/features-extra-net-auth/register-number/register-number.tsx
+++ src/features/features-extra-net-auth/register-number/register-number.tsx
@@ -11,8 +11,37 @@ const RegisterNumber = () => {
                     </h1>
                 </div>
                <RenderButtons />
+                <div className={style.input}>
+                    <div className={style.inputInfoTitle}>
+                        <p className={style.inputInfoTitleInner}>
+                            Телефон
+                        </p>
+                    </div>
+                    <div className={style.inputInfoTextWrapper}>
+                        <div className={style.inputInfo}>
+                            <input className={style.inputInner} type="text" />
+                        </div> 
+                    </div>
+                </div>
+            <div className={style.button}>
+                <button className={style.buttonInner}>
+                    Продолжить
+                </button>
+            </div>
+            <div className={style.description}>
+                <div className={style.leftPart}>
+                    <p className={style.descriptionInner}>
+                        Уже зарегистрированы?
+                    </p>
+                </div>
+                <div className={style.rightPart}>
+                    <p className={style.descriptionInnerRight}>
+                        Войти
+                    </p>
+                </div>
             </div>
         </div>
+    </div>
     )
 }
 
