7c55bd4faf95113805c8521c2455cc5670ecfe07
dantauu <azazzzz455u@gmail.com>
2025-03-22 19:54:41 +0300

    fix

--- next.config.ts
+++ next.config.ts
@@ -1,28 +1,12 @@
-import { STEPS } from "@/config/step";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-	compilerOptions: {
-		strict: true,
-		esModuleInterop: true,
-		lib: ['dom', 'dom.iterable', 'esnext'],
-		skipLibCheck: true,
-	},
-	eslint: {
-		ignoreDuringBuilds: true,
-	},
-	images: {
-		disableStaticImages: true,
-	},
-  async redirects() {
-    return [
-      {
-        source: '/add-card-object',
-        destination: STEPS[0],
-        permanent: false,
-      }
-    ]
-  }
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    disableStaticImages: true,
+  },
 }
 
 export default nextConfig;
